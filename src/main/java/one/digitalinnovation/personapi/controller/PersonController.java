af753c9561a94765c439ddfbefda520291d39f93
Paulovca <paulo.c.aureliano@gmail.com>
2021-07-12 17:12:20 -0300

    Fix in PersonController

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,4 +1,4 @@
-package com.github.heberleandro.personapi.controller;
+package one.digitalinnovation.personapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
